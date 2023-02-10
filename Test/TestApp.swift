51be75960e39a385908e77a7cc66d0af368c318d
Sufian Idris <sufian.idris@gmail.com>
2023-02-10 11:16:22 +0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Sufian Bin Idris on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
